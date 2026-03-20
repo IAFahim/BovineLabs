ba0e01285b0a3c09c9c5c1c91deb8f484a0240df
IAFahim <ishtiaq.fahim@northsouth.edu>
2026-03-20 21:11:24 +0600

    Initial commit

---
+++ Assets/Scripts/Scripts.Data/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+// <copyright file="AssemblyInfo.cs" company="DefaultCompany">
+// Copyright (c) DefaultCompany. All rights reserved.
+// </copyright>
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Scripts")]
+[assembly: InternalsVisibleTo("Scripts.Authoring")]
+[assembly: InternalsVisibleTo("Scripts.Debug")]
+[assembly: InternalsVisibleTo("Scripts.Editor")]
+[assembly: InternalsVisibleTo("Scripts.Tests")]
