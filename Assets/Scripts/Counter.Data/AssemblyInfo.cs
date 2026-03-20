b479621444205603c103c721e729ea62e9aa3f38
IAFahim <ishtiaq.fahim@northsouth.edu>
2026-03-20 18:27:29 +0600

    removed unwanted files from example

--- Assets/Scripts/Counter.Data/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Counter")]
-[assembly: InternalsVisibleTo("Counter.Authoring")]
-[assembly: InternalsVisibleTo("Counter.Tests")]
--- Assets/Scripts/Counter.Data/Components/CounterComponent.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Entities;
-
-namespace Counter.Data.Components
-{
-    public struct CounterComponent : IComponentData
-    {
-        public int Value;
-    }
-}
